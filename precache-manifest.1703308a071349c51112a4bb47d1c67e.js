34560bf4deed270f20356056964c5bc00478a443
Robel Tessema <robeltessema.h@gmail.com>
2020-09-07 14:52:50 -0500

    Updates

---
+++ precache-manifest.1703308a071349c51112a4bb47d1c67e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "acf9349078c2dd790506601017524c56",
+    "url": "/deploy-react/index.html"
+  },
+  {
+    "revision": "f9f99e65baa921694ea9",
+    "url": "/deploy-react/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "440847822ffdbbe6ad72",
+    "url": "/deploy-react/static/js/2.fc3e5acd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/deploy-react/static/js/2.fc3e5acd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9f99e65baa921694ea9",
+    "url": "/deploy-react/static/js/main.77305cf4.chunk.js"
+  },
+  {
+    "revision": "3956da8098b50176870b",
+    "url": "/deploy-react/static/js/runtime-main.efa8a6ad.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/deploy-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
